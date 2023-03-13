de5d18f05c6902926812faba5b7c41b226f1d335
Mohit Patel <hydramcpatel@gmail.com>
2023-03-13 09:36:12 +0530

    1st Vs Pro

--- vs code/1st Project.py
+++ vs code/1st Project.py
@@ -1,13 +1,13 @@
 import pyautogui as pg
 import time
 
-good=(input("Enter the message you want to send:" ))
+msg=(input("Enter the message you want to send:" ))
 r=int(input("Enter the quantity you want to send: "))
 
 if(r>0):
     time.sleep(5)
     for i in range(r):
-         g=good
+         g=msg
          pg.write(g)
          pg.press("enter") 
 else:
